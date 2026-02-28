2f0af50ad4e04bc6787c28da929ba8175b3e97c1
shubhamkumar0114 <shubham75jha@gmail.com>
2026-03-01 00:14:06 +0530

    backends

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://chating-app-bice.vercel.app"],
-    method: ["POST", "GET", "DELETE", "PUT"],
+    origin: "https://chating-app-bice.vercel.app",
+    methods: ["POST", "GET", "DELETE", "PUT"],
     credentials: true,
   },
 });
