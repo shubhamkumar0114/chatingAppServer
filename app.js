38205d14dc5e9e1f6499eeed46379cc44552f602
shubhamkumar0114 <shubham75jha@gmail.com>
2026-02-28 19:02:45 +0530

    update url

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://chating-app-bice.vercel.app"],
     method: ["POST", "GET", "DELETE", "PUT"],
     credentials: true,
   },
@@ -47,7 +47,7 @@ cloudinary.config({
 // middlewares
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://chating-app-bice.vercel.app"],
     method: ["POST", "GET", "DELETE", "PUT"],
     credentials: true,
   }),
