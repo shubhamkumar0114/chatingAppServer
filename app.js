f64391f25765c63769e4089a16e52f24975748fe
shubhamkumar0114 <shubham75jha@gmail.com>
2026-03-01 13:18:46 +0530

    backend

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://chating-app-wine.vercel.app"],
     methods: ["POST", "GET", "DELETE", "PUT"],
     credentials: true,
   },
@@ -46,7 +46,7 @@ cloudinary.config({
 // middlewares
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://chating-app-wine.vercel.app"],
     methods: ["POST", "GET", "DELETE", "PUT"],
     credentials: true,
   }),
